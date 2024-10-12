ccd19373f4c8bf8aa82aa347c2f60a13b023b86b
1nfer.exe <InferKing@users.noreply.github.com>
2024-10-12 14:33:22 +0500

    fix api.py

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
 import requests
 import pandas as pd
 from numpy import nan
+from random import uniform
+from time import sleep
+from datetime import datetime
 
 
 class HHParser:
@@ -9,65 +12,78 @@ class HHParser:
         self.__url_by_id = self.__base_url + "/{id}"
 
         self.__raw_json = None
-        self.__df = None
-
-    @property
-    def dataframe(self) -> pd.DataFrame:
-        return self.__df
+        self.__base_match_fields = [
+            {"field": "Подробнее", "cellRenderer": "GetMoreInfo", "sortable": False, "width": 180},
+            {"field": "Название вакансии", "cellRenderer": "VacancyLink", "filter": True},
+            {"field": "Дата публикации", "filter": True, "width": 150},
+            {"field": "Регион", "filter": True},
+            {"field": "Зарплата от", "cellDataType": "number", "filter": True, "width": 130},
+            {"field": "Зарплата до", "cellDataType": "number", "filter": True, "width": 130},
+            {"field": "Валюта", "filter": True, "width": 90},
+            {"field": "Компания", "filter": True},
+            {"field": "Опыт работы", "filter": True},
+            {"field": "График работы", "filter": True},
+            {"field": "Тип занятости", "filter": True}
+        ]
 
 
     @property
     def grid_fields(self) -> list[dict]:
-        return [{"field": field} for field in self.dataframe.columns]
+        return self.__base_match_fields
+
+
+    @property
+    def grid_fields_by_id(self) -> list[dict]:
+        return self.__base_match_fields[1:]
 
 
     def make_request_all(self, **kwargs) -> pd.DataFrame:
         kwargs["per_page"] = 100
         max_pages = kwargs.get("max_pages", 10)
+        df = pd.DataFrame()
 
         try:
             for page in range(max_pages):
                 kwargs["page"] = page
                 response = requests.get(self.__base_url, params=kwargs)
-            self.__raw_json = response.json()
-            self.__df = pd.concat([self.__df, self.__make_df()], axis=0, ignore_index=True)
+                self.__raw_json = response.json()
+                df = pd.concat([df, self.__make_df()], axis=0, ignore_index=True)
+
+                sleep(uniform(0.5, 1))
         except requests.HTTPError as e:
             print(e)
 
-        return self.__df
+        return df
 
 
     def make_request_by_id(self, id: str) -> pd.DataFrame:
+        # TODO: Add error handling
         response = requests.get(self.__url_by_id.format(id=id))
         self.__raw_json = response.json()
 
-        picked_row = self.__find_row_by_id(id).reset_index(drop=True)
         picked_data = self.__make_df(by_id=True)
-        d = pd.concat([picked_data, picked_row], axis=1)
-        return d
+        return picked_data
     
-    def __find_row_by_id(self, id: str) -> pd.DataFrame:
-        return self.dataframe[self.dataframe["ID"] == id]
+    # def __find_row_by_id(self, id: str) -> pd.DataFrame:
+    #     return self.dataframe[self.dataframe["ID"] == id]
 
 
     def __make_df(self, by_id: bool = False) -> pd.DataFrame: 
-        json_data = self.__raw_json.get("items", self.__raw_json)
+        json_data = self.__raw_json.get("items", [self.__raw_json])
         df = pd.DataFrame()
 
         if by_id:
-            df["Описание"] = [json_data["description"]]
+            df["Описание"] = [json_data[0]["description"]]
             data = ""
 
-            if json_data.get("key_skills"):
-                for skill in json_data["key_skills"]:
+            if json_data[0].get("key_skills"):
+                for skill in json_data[0]["key_skills"]:
                     data += skill["name"] + ", "
 
                 data = data[:-2]
                 df["Навыки"] = [data]
             else:
                 df["Навыки"] = "Не указано"
-
-            return df
 
         simple_columns = {
             "id": "ID",
@@ -88,17 +104,21 @@ class HHParser:
             "experience": {"name": "Опыт работы"},
             "employment": {"name": "Тип занятости"}
         }
-
         for key, value in simple_columns.items():
             df[value] = [item.get(key, "Не указано") for item in json_data]
 
         for field, columns in separate_columns.items():
             for key, value in columns.items():
                 df[value] = [
-                    item.get(field, {}).get(key, "Не указано") if item.get(field) is not None else "Не указано"
+                    item.get(field, {}).get(key, nan) if item.get(field) is not None else nan
                     for item in json_data
                 ]
+
         df["Зарплата от"] = df["Зарплата от"].replace({None: nan})
         df["Зарплата до"] = df["Зарплата до"].replace({None: nan})
+
+        df["Дата публикации"] = df["Дата публикации"].apply(lambda x: datetime.fromisoformat(x[:-5]).strftime("%Y-%m-%d"))
+        df["Зарплата от"] = df["Зарплата от"].astype(float)
+        df["Зарплата до"] = df["Зарплата до"].astype(float)
 
         return df
